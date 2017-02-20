d837c32e9335877fe3d0fae8635bfe070064f96e
ZhengFan <zhengfan@foundersc.com>
2017-02-20 15:43:41 +0800

    Commit for fopen.

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "468feba"
-#define MARS_PATH "master"
+#define MARS_REVISION "b5cf235"
+#define MARS_PATH "new_openssl"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-01-31 23:30:36"
+#define MARS_BUILD_TIME "2017-02-14 16:59:00"
 #define MARS_TAG "default"
 
 #endif
